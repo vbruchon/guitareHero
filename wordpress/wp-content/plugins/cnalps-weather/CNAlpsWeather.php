61e3920ae9b43d63eaf3437cc84ea85831672647
root <vivian.bruchon@le-campus-numrique.fr>
2022-09-27 14:33:17 +0200

    Simple Weather Widget

--- wordpress/wp-content/plugins/cnalps-weather/CNAlpsWeather.php
+++ wordpress/wp-content/plugins/cnalps-weather/CNAlpsWeather.php
@@ -9,21 +9,22 @@
  */
 
 
-class CNAlpsWeather extends Wp_Widget
+class CNAlpsWeather extends WP_Widget
 {
     function __construct()
     {
         $options = array(
-    		'classname'   => 'CNAlpsWeather',
-    		'description' => 'Minimalistic weather widget'
-    	);
+            'classname'   => 'CNAlpsWeather',
+            'description' => 'Minimalistic weather widget'
+        );
         parent::__construct(
             /* Widget ID */
             'CNAlpsWeather',
             /* Widget Name */
             'CNAlps Weather Widget',
             /* Widget Description/options */
-             $options);
+            $options
+        );
     }
 
     public function widget($args, $instance)
@@ -34,15 +35,45 @@ class CNAlpsWeather extends Wp_Widget
         if (!empty($title))
             echo $args['before_title'] . $title . $args['after_title'];
         //output
-        echo __('Bienvenue sur le widget CNAlps Weather', 'text_domain');
-
+        echo __(
+            '<div class="cnalps-weather-widget"> 
+            <div class="weather-title">La météo à' . " " . $instance['city'] . ' en ' . $instance['country'] . '</div>
+        </div>'
+        );
         echo $args['after_widget'];
     }
+
+    public function form($instance)
+    {
+        $city = $instance['city'];
+        $country = $instance['country'];
+?>
+
+        <p>
+            <label for="<?php echo $this->get_field_id('city'); ?>"><?php _e('City:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('city'); ?>" name="<?php echo $this->get_field_name('city'); ?>" type="text" value="<?php echo esc_attr($city); ?>" />
+
+            <label for="<?php echo $this->get_field_id('country'); ?>"><?php _e('Pays:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('country'); ?>" name="<?php echo $this->get_field_name('country'); ?>" type="text" value="<?php echo esc_attr($country); ?>" />
+        </p>
+
+<?php
+    }
+
+    public function update($new_instance, $old_instance)
+    {
+        $instance = array();
+
+        $instance['city'] = (!empty($new_instance['city'])) ? strip_tags($new_instance['city']) : '';
+        $instance['country'] = (!empty($new_instance['country'])) ? strip_tags($new_instance['country']) : '';
+
+        return $instance;
+    }
 }
+
 function cnalps_register_weather_widget()
 {
     register_widget('CNAlpsWeather');
 }
 
 add_action('widgets_init', 'cnalps_register_weather_widget');
-
